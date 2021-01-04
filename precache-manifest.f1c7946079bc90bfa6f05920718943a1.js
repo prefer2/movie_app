17c69f68d9c2f5d6c14980c8e6645124a90ea9a8
prefer2 <sunho6200@gmail.com>
2021-01-04 21:19:33 +0900

    Updates

---
+++ precache-manifest.f1c7946079bc90bfa6f05920718943a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd60d8ab9e2d74251ad4b3a4b5fe794",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "82ac2252167159765de6",
+    "url": "/movie_app/static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "2baf1e1f5ab767ca97cc",
+    "url": "/movie_app/static/js/2.3033b0ce.chunk.js"
+  },
+  {
+    "revision": "82ac2252167159765de6",
+    "url": "/movie_app/static/js/main.4d07ecec.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
